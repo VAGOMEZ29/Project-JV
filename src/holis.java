edcd33463aea3727f9ad99360eb84ce21e4252ca
VAGOMEZ29 <veaguirreg@unal.edu.co>
2025-05-17 21:13:47 -0500

    CAMBIO message

--- src/holis.java
+++ src/holis.java
@@ -1,5 +1,5 @@
 public class holis {
     public void main(String[] args) {
-        System.out.println("Hola Vero");
+        System.out.println("Hola la facuamiga de la vida que voy a conocer dentro de poco tiempo  ATT: ESTEBAN CORTES" );
     }
 }
